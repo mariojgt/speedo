64783be25d74e4c42256dc35432db126828cf0a3
mario jose goes tarosso <mariojgt2@gmail.com>
2021-07-28 16:56:07 +0100

    need fix blade compier

--- src/config/app.php
+++ src/config/app.php
@@ -2,6 +2,6 @@
 
 return [
     'base_route'     => 'speedo',                      // Normaly is the folder name
-    'base_view_path' => 'src/views/',                      // Normaly is the folder name
+    'base_view_path' => 'src/App/views/',                      // Normaly is the folder name
     'url'            => 'http://localhost/deposit/',   // Please change this to the current website up to the root folder
 ];

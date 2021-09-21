3a9b54e6694eeb3fa9506434736eefb9d385b7e0
Mario Jose Goes Tarosso <mariojgt2@gmail.com>
2021-09-21 13:30:40 +0100

    Update app.php

--- src/config/app.php
+++ src/config/app.php
@@ -3,6 +3,6 @@
 return [
     // Normaly is the folder nama, this will be use create a routes name ignoring the first part of the url
     'base_route'     => 'speedo',
-    'base_view_path' => 'src/App/views/',                      // Normaly is the folder name
+    'base_view_path' => getcwd().'/src/App/Views',     // Normaly is the folder name
     'url'            => 'http://localhost/deposit/',   // Please change this to the current website up to the root folder
 ];

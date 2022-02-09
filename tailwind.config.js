bda731f8d00f48a1b20212aedee29cc569b3e1c3
mario jose goes tarosso <mariojgt2@gmail.com>
2022-02-09 10:57:35 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,23 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-    darkMode: 'class',
-    mode: 'jit',
+    content: [
+      "./**/*.{html,js,vue,blade}",
+      "./**/*.{html,js,vue,blade}"],
+    darkMode: "class",
+    mode: "jit",
     purge: [
         // Path to my php view it will only purge stuf we goin to use
         "/src/App/Views/**/*.php",
         "/resources/js/**/*.vue",
     ],
     theme: {
-      extend: {},
+        extend: {},
     },
     variants: {
         extend: {
-          textOpacity: ['dark']
-        }
+            textOpacity: ["dark"],
+        },
     },
     plugins: [],
-  }
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,29 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
+const path = require("path");
+
+mix.webpackConfig({
+    module: {
+        rules: [
+            // Add support for Vue ref sugar (let ref = $ref())
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+                options: {
+                    reactivityTransform: true,
+                },
+            },
+            // Add support for <style lang="postcss"> blocks
+            {
+                test: /\.(postcss)$/,
+                use: [
+                    "vue-style-loader",
+                    { loader: "css-loader", options: { importLoaders: 1 } },
+                    "postcss-loader",
+                ],
+            },
+        ],
+    },
+});
 
 /*
  |--------------------------------------------------------------------------
@@ -10,22 +35,20 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+const basPath = path.resolve(__dirname, "/");
 
-// Normal js files
-mix.js('src/resources/js/app.js', 'public/speedo/js')
-    .sourceMaps()
-    .version();
+// Compile the js file using the base path
+mix.js(`src/resources/js/app.js`, `public/speedo/js`).sourceMaps().version();
 
 // Vue js example
-mix.js('src/resources/js/vue.js', 'public/speedo/js')
-    .vue({version: 3})
+mix.js(`src/resources/js/vue.js`, `public/speedo/js`)
+    .vue({ version: 3 })
     .sourceMaps()
     .version();
 
-const tailwindcss = require('tailwindcss')
+const tailwindcss = require("tailwindcss");
 
-mix.sass('src/resources/sass/app.scss', 'public/speedo/css')
-   .options({
-      processCssUrls: false,
-      postCss: [ tailwindcss('tailwind.config.js') ],
+mix.sass(`src/resources/sass/app.scss`, `public/speedo/css`).options({
+    processCssUrls: false,
+    postCss: [tailwindcss("tailwind.config.js")],
 });
